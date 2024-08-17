995f9897b1c73d9e82106344e6c14fed5ff57113
Fernando Futila <fernando.futila@gmail.com>
2024-08-17 14:59:04 -0300

    feat: UI base structure

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,29 +1,14 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import {TypedUseSelectorHook, useSelector} from "react-redux"
 
 
-const todoSlice = createSlice({
-  name: "todo", 
-  initialState: ["Estudar Javascript", "Estudar Zustand"], 
 
-
-  reducers: {
-    add: (state, action) => {
-        state.push(action.payload.newTodo)
-    }
-  }
-})
 
 export const store = configureStore({
   reducer: {
-    todo: todoSlice.reducer
+   
   }
 })
-
-
-export const {add} = todoSlice.actions
-
-
 
 export type RootState = ReturnType<typeof store.getState>
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
