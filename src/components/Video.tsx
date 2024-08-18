e743ac71c3525c2a448871d3be28a8e5851fc2b7
Fernando Futila <fernando.futila@gmail.com>
2024-08-18 18:08:11 -0300

    feat: add global hook to retrive the cuurent lesson and module

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,18 +1,10 @@
-import ReactPlayer from 'react-player'
-import { useAppSelector } from '../store';
+import ReactPlayer from 'react-player';
 import { useDispatch } from 'react-redux';
-import { next } from '../store/slices/player';
+import { next, useCurrentLesson } from '../store/slices/player';
 
 export function Video() {
   const dispatch = useDispatch()
-  const lesson = useAppSelector((state) => {
-    
-    const {currentLessonIndex, currentModuleIndex} = state.player
-
-    const currentLesson = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
-
-    return currentLesson
-  })
+  const {currentLesson} = useCurrentLesson()
 
 
   function handlePlayNext() {
@@ -26,7 +18,7 @@ export function Video() {
         controls
         playing
         onEnded={handlePlayNext}
-        url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
       />
     </div>
   );
